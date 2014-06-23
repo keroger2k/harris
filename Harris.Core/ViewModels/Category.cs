0b4da99434f25c608e54f6dd88388eb964209142
unknown <kroger03@SL013294.cs.myharris.net>
2014-06-23 16:14:34 -0500

    init plus one

---
+++ Harris.Core/ViewModels/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harris.Core.ViewModels {
+  public class Category {
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public IEnumerable<Capability> Capabilities { get; set; }
+  }
+
+  public class Capability {
+    public int Id { get; set; }
+    public string Name { get; set; }
+  }
+
+}
--- Harris.Web/Controllers/HomeController.cs
+++ Harris.Web/Controllers/HomeController.cs
@@ -1,20 +1,63 @@
-﻿using System;
+﻿using Harris.Core.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace Harris.Web.Controllers
-{
-    public class HomeController : Controller
-    {
-        //
-        // GET: /Home/
+namespace Harris.Web.Controllers {
+  public class HomeController : Controller {
+    //
+    // GET: /Home/
 
-        public ActionResult Index()
-        {
-            return View();
-        }
-
+    public ActionResult Index() {
+      var items = new List<Category>();
+      items.AddRange(new List<Category> { 
+        new Category { 
+          Id  = 1, 
+          Name = "Information Assurance", 
+          Capabilities = new List<Capability> {
+            new Capability { 
+              Id = 1,
+              Name = "C&A (DISA STIGs, DIACAP, FISMA"
+            },
+            new Capability { 
+              Id = 2,
+              Name = "Information Assurance Vulnerability Alert (IAVA)"
+            },
+            new Capability { 
+              Id = 3,
+              Name = "Configuration Control & Management"
+            },
+            new Capability { 
+              Id = 4,
+              Name = "Physical Security"
+            }
+        }},
+        new Category { 
+          Id  = 2, 
+          Name = "IT Services", 
+          Capabilities = new List<Capability> {
+            new Capability { 
+              Id = 1,
+              Name = "Change Management"
+            },
+            new Capability { 
+              Id = 2,
+              Name = "Configuration Management"
+            },
+            new Capability { 
+              Id = 3,
+              Name = "Capacity Planning"
+            },
+            new Capability { 
+              Id = 4,
+              Name = "Computer-Aided Design/Computer-Aided Manufacturing (CAD/CAM)"
+            }
+        }}
+      });
+      return View(items);
     }
+
+  }
 }
